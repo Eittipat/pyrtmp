ac7b49abeed46925aee56a2e9ee1a89755c7393a
Eittipat.K <iammop@gmail.com>
2023-12-10 17:54:25 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,20 +4,17 @@ from setuptools import find_packages
 setup(
     name='pyrtmp',
     packages=find_packages(),
-    version='0.2.0',
+    version='0.3.0',
     license='MIT',
-    description='Pure python RTMP server',
+    description='PyRTMP: Pure Python RTMP server',
     author='Eittipat.K',
     author_email='iammop@gmail.com',
     url='https://github.com/Eittipat/pyrtmp.git',
-    download_url='https://github.com/Eittipat/pyrtmp/releases/tag/v0.2.0',
+    download_url='https://github.com/Eittipat/pyrtmp/releases/tag/v0.3.0',
     keywords=['RTMP', 'RTMPT', 'asyncio'],
-    python_requires='>=3',
+    python_requires='>=3.10',
     install_requires=[
-        'aiounittest>=1.4.0',
-        'bitstring>=3.1.7',
-        'Quart>=0.15.1',
-        'psutil>=5.8.0',
+        'bitstring>=4'
     ],
     classifiers=[
         'Development Status :: 5 - Production/Stable',
